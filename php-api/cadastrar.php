510fef80d2813deaddec3f6bfb19b7ef9dfe8318
Tainá Miranda <tainamirandaferreira@gmail.com>
2022-09-23 19:10:52 -0300

    enviando

--- php-api/cadastrar.php
+++ php-api/cadastrar.php
@@ -2,11 +2,46 @@
 //criando cabelho para quem que ter acesso na api
 
 header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset-UTF-8");
 
 header("Access-Control-Allow-Headers: *");
-header("Access-Control-Allow-Headers: GET,PUT,DELETE,POST");
+//header("Access-Control-Allow-Headers: GET,PUT,DELETE,POST");
 
 //formatos qye quero receber
-header("Content-Type: application/json; charset-UTF-8");
 //inculuido conexão
 include_once 'conexao.php';
+
+$response_json=file_get_contents("php://input");
+//lendo
+ $dados= json_decode($response_json, true);
+
+
+if($dados){
+   $query_produto="INSERT INTO produtos (titulo, descricao) VALUES (:titulo, :descricao)";
+   $car_produto= $conn->prepare($query_produto);
+   
+   $car_produto->bindParam(':titulo', $dados['produto']['titulo']);
+   $car_produto->bindParam(':descricao', $dados['produto']['descricao']);
+   $car_produto->execute();
+   //verificar
+   if($car_produto->rowCount()){
+    $response=[
+        "erro"=>false,
+        "messagem"=>"Produto cadastrado"
+    ];
+   }else{
+    $response=[
+        "erro"=>true,
+        "messagem"=>"Produto não cadastrado"
+    ];
+   }
+   
+}else{
+    $response=[
+        "erro"=>true,
+        "messagem"=>"Produto não cadastrado"
+    ];
+}
+
+http_response_code(200);
+echo json_encode($dados);
